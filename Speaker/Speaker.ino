969b570048b018262b32c465e672dbdd80cecdda
Harry P <159206602+PDAHarry10@users.noreply.github.com>
2024-11-21 10:30:25 +1100

    fixed sound

--- Speaker/Speaker.ino
+++ Speaker/Speaker.ino
@@ -1,32 +1,31 @@
-//Set up Speaker and set to pin 5
 
+// Documentation:
+// https://wiki.seeedstudio.com/Grove-Speaker/
+
+//Set up Speaker and set to pin 5
 #define SPEAKER 5
 
-//Currently unknown
-int BassTab[]={1911};//bass 1~7
+//Set up Bool check
+
+//Set Bass Sound
+int BassTab[]={1911};
 
 //Setup function
 void setup()
 {
-    pinInit();
-}
-void loop()
-{
-        //play sound from notes 1 -7
-    for(int note_index=0;note_index<7;note_index++)
-    {
-        sound(note_index);
-        delay(500);
-    }
-}
-void pinInit()
-{
     pinMode(SPEAKER,OUTPUT);
     digitalWrite(SPEAKER,LOW);
 }
+//Start Loop
+void loop()
+{
+  sound(0);
+  delay(500);
+}
+
 void sound(uint8_t note_index)
 {
-    for(int i=0;i<100;i++)
+    for(int i=0;i<10;i++)
     {
         digitalWrite(SPEAKER,HIGH);
         delayMicroseconds(BassTab[note_index]);
