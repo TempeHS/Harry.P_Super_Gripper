703c20d6fe64c908cb7ac01843bd2280f1e3def1
Harry P <159206602+PDAHarry10@users.noreply.github.com>
2024-11-20 12:22:38 +1100

    Added Research to Main File

---
+++ SuperGripper/SuperGripper.ino
@@ -0,0 +1,8 @@
+// Ultrasonic Sensor:
+
+// Servo Motor:
+// https://github.com/TempeHS/TempeHS_Ardunio_Boilerplate/tree/main/TempeHS_Sensor_Catalogue/Sensor_Kit/Servo_Motor
+// Speaker:
+// https://wiki.seeedstudio.com/Grove-Speaker/
+// LED Light:
+// https://github.com/TempeHS/TempeHS_Ardunio_Boilerplate/tree/main/TempeHS_Sensor_Catalogue/Sensor_Kit/Red_LED
